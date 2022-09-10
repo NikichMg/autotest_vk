9de2a35ac1bdba5c705cca020e59d52d4a0bd04a
Пользователь <https://github.com/NikichMg/autotest_stipik>
2022-09-10 20:51:10 +0700

    new

--- test_1.py
+++ test_1.py
@@ -5,10 +5,23 @@ def test_create_tuple_structure():
     logging.info('ТЕСТ НА КОРРЕКТНОСТЬ СОЗДАНИЯ ТИПА ДАННЫХ TUPLE')
     logging.debug('Создаем структуру данных типа tuple')
     a = tuple()
-    logging.debug('Проверяем получившейся тип данных(должен принадлежать:tuple)')
+    logging.debug('Проверяем получившейся тип данных')
     assert f"{type(a)}" == "<class 'tuple'>", logging.error('FAILED')
     logging.info('PASSED')
 
-def test_add_new_items_tuple_structure():
-    logging.info('ТЕСТ НА КОРРЕКТНОСТЬ ДОБОВЛЕНИЯ НОВЫХ ЭЛЕМЕНТОВ В КАРТЕЖ')
-    logging.debug('Создаем структуру данных типа tuple')
+def test_converting_list_to_tuple():
+    tpl = (2, 4, 6, 8, 10)
+    lst = list(tpl)
+    assert f"{type(lst)}" == "<class 'list'>", logging.error('FAILED')
+
+def test_converting_tuple_to_list():
+    lst = [1, 2, 3, 4, 5]
+    tpl = tuple(lst)
+    assert f"{type(tpl)}" == "<class 'tuple'>", logging.error('FAILED')
+
+
+def test_zero_division_float_structure():
+
+
+
+
