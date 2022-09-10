8d4b05cb98b68e73dff6b3907ea2c1b3c8dc89d0
Пользователь <https://github.com/NikichMg/autotest_stipik>
2022-09-10 19:40:44 +0700

    Initial commit

---
+++ test_1.py
@@ -0,0 +1,8 @@
+import pytest
+import logging
+
+def test_create_tuple_structure():
+    logging.info('ТЕСТ НА КОРРЕКТНОСТЬ СОЗДАНИЯ ТИПА ДАННЫХ TUPLE')
+    logging.debug('Создаем структуру данных типа tuple')
+    a = tuple()
+    assert
